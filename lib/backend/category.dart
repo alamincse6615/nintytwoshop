696c7a97445b4af99315167f1c826a9abae74b4e
mdyasinkhan1996 <mdyasinkhan603@gmail.com>
2022-01-25 13:18:22 +0600

    new Update

--- lib/backend/category.dart
+++ lib/backend/category.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:ninty_towshop/backend/addCategory.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
+import 'package:firebase_database/firebase_database.dart';
 
 class Category extends StatefulWidget {
   const Category({Key? key}) : super(key: key);
@@ -9,8 +11,17 @@ class Category extends StatefulWidget {
 }
 
 class _CategoryState extends State<Category> {
+
+  late DatabaseReference _databaseReference;
+  @override
+  void initState() {
+    _databaseReference = FirebaseDatabase.instance.reference();
+  }
+
+
   @override
   Widget build(BuildContext context) {
+    final category = _databaseReference.child("subCategory");
     return Scaffold(
       appBar: AppBar(
         title: Text("Category"),
@@ -30,6 +41,39 @@ class _CategoryState extends State<Category> {
                   );
                 },
               ),
+            ),
+          ),
+          Container(
+            child: FirebaseAnimatedList(
+                 query: category,
+                itemBuilder: (BuildContext context, DataSnapshot snapshot, Animation<double> animation, int index){
+                  return Card(
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            IconButton(
+                                onPressed: (){},
+                                icon: Icon(
+                                  Icons.edit
+                                ),
+                            ),
+                            IconButton(
+                                onPressed: (){},
+                                icon: Icon(
+                                  Icons.delete
+                                ),
+                            ),
+                          ],
+                        ),
+                        Container(
+                          height: 2,
+                          color: Colors.black26,
+                        )
+                      ],
+                    ),
+                  );
+                }
             ),
           )
         ],
--- lib/backend/subCategory.dart
+++ lib/backend/subCategory.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:flutter/material.dart';
 import 'package:ninty_towshop/backend/addSubCategory.dart';
+import 'package:firebase_database/firebase_database.dart';
 
 class subCategory extends StatefulWidget {
   const subCategory({Key? key}) : super(key: key);
@@ -9,8 +11,16 @@ class subCategory extends StatefulWidget {
 }
 
 class _subCategoryState extends State<subCategory> {
+
+  late DatabaseReference _databaseReference;
+  @override
+  void initState() {
+    _databaseReference = FirebaseDatabase.instance.reference();
+  }
+  
   @override
   Widget build(BuildContext context) {
+    final subProduct = _databaseReference.child("subCategory");
     return Scaffold(
       appBar: AppBar(
         title: Text("Sub Category"),
@@ -18,6 +28,7 @@ class _subCategoryState extends State<subCategory> {
       body: Column(
         children: [
           Container(
+            height: 100,
             child: Center(
               child: ElevatedButton(
                 child: Text("Add Category"),
@@ -31,6 +42,25 @@ class _subCategoryState extends State<subCategory> {
                 },
               ),
             ),
+          ),
+          Container(
+            height: 500,
+            child: FirebaseAnimatedList(
+                query: subProduct,
+                itemBuilder: (BuildContext context, DataSnapshot snapshot, Animation<double> animation, int index){
+                  return Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Card(
+                      child: Column(
+                        children: [
+                          Text(snapshot.child("cat_id").value.toString()),
+                          Text(snapshot.child("subCategoryNameBn").value.toString())
+                        ],
+                      ),
+                    ),
+                  );
+                },
+            )
           )
         ],
       ),
